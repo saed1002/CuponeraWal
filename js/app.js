14aa62ae40f8080d0630ab9ec2450e7ae7974f12
Saed1002 <eduardoquet@hotmail.com>
2021-06-05 05:33:15 -0500

    demo

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 //conexion a la bd de firebase
 const db =firebase.firestore()
 
+//variable que guarda informacion del usuario logeado
 let infoUser={name:'',email:''}
 
 //mandar a llamar una bd
 const usuario = db.collection("Usuarios");
 
+//Obtener informacion del usuario logeado
 auth.onAuthStateChanged(usr=>{infoUser.email=usr.email,infoUser.name=usr.displayName})
+
+//crear coleccion 
 const createUser ={
     name: infoUser.name,
     points: 10,
@@ -14,5 +18,6 @@ const createUser ={
     phone: "55-55-55-55-55",
     mail: infoUser.email,
 }
-const newUser = await usuario.add(createUser);
+ //montar
+const newUser = usuario.add(createUser);
 console.log(newUser.name);
