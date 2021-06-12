e26230cb4de4826aa99e14ec7a8fb405fa84c583
Saed1002 <eduardoquet@hotmail.com>
2021-06-12 05:28:05 -0500

    V 0.0.2.1 Navar demo 1.3

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+document.getElementById("navar").innerHTML+=`
+<div class="container-fluid">
+      <a class="navbar-brand" href="./index.html">
+        <img src="http://1000marcas.net/wp-content/uploads/2020/02/Walmart-logo.png" alt="imagen-walmart" width="90" height="40">
+      </a>
+      <a class="navbar-brand" href="./promociones.html">Promociones</a>
+      <a class="navbar-brand" href="./estadisticas">Estadisticas</a>
+      <button class="navbar-brand" type="button" onclick="terminaSesión()">Terminar Sesión</button>
+</div>
+`;
