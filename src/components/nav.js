ec3e023327e5e4d19303b5e229b41ba6f663d67e
Saed1002 <eduardoquet@hotmail.com>
2021-06-13 13:24:32 -0500

    V 0.0.1.3 iconos cdn change  1.13

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,26 +1,29 @@
 
 auth.onAuthStateChanged(usr=>{infoUser.email=usr.email,infoUser.name=usr.displayName})
-if(infoUser.email==="406474058038.cuponera@gmail.com"){
+
+var urlNav={promotions: "",estadistics: ""}
+
+navar(infoUser.email)
+
+function navar(email){
+  if(email==="406474058038.cuponera@gmail.com"){
+    urlNav.estadistics="./estadisticas.html"
+    urlNav.promotions="./promociones.html"
+  }
+  else{
+    urlNav.estadistics="./estadisticasUsuario.html"
+    urlNav.promotions="./promocionesUsuario.html"
+  }
+}
+
 document.getElementById("navar").innerHTML+=`
 <div class="container-fluid">
       <a class="navbar-brand" href="./index.html">
         <img src="http://1000marcas.net/wp-content/uploads/2020/02/Walmart-logo.png" alt="imagen-walmart" width="90" height="40">
       </a>
-      <a class="navbar-brand" href="./promociones.html">Promociones</a>
-      <a class="navbar-brand" href="./estadisticas">Estadisticas</a>
+      <a class="navbar-brand" href="${urlNav.promotions}">Promociones</a>
+      <a class="navbar-brand" href="${urlNav.estadistics}">Estadisticas</a>
       <button class="navbar-brand btn btn btn-link" type="button" onclick="terminaSesión()">Terminar Sesión</button>
 </div>
 `;
-}
-else{
-  document.getElementById("navar").innerHTML+=`
-    <div class="container-fluid">
-          <a class="navbar-brand" href="./index.html">
-            <img src="http://1000marcas.net/wp-content/uploads/2020/02/Walmart-logo.png" alt="imagen-walmart" width="90" height="40">
-          </a>
-          <a class="navbar-brand" href="./promocionesListado.html">Promociones</a>
-          <a class="navbar-brand" href="./estadisticas">Estadisticas</a>
-          <button class="navbar-brand btn btn btn-link" type="button" onclick="terminaSesión()">Terminar Sesión</button>
-    </div>
-  `;
-}
+  
