9e6479c46d93b903707b491d7b2e0f73f41e0bd5
Saed1002 <eduardoquet@hotmail.com>
2021-06-13 16:05:11 -0500

    V 0.0.1.3 iconos cdn change  1.16

--- src/conection.js
+++ src/conection.js
@@ -9,6 +9,14 @@
     appId: "1:406474058038:web:62a527c117af27f3b52b97",
     measurementId: "G-WEEL5GKJB2"
   };
+/*cambios en firebase rules_version = '2';
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
+      allow read, write: if request.auth.uid != null;
+    }
+  }
+}*/
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
