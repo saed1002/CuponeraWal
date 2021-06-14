c8f8e1747cfd0ce0cfa59f1d67f77a224b0f20e7
Saed1002 <eduardoquet@hotmail.com>
2021-06-13 20:15:07 -0500

    V 0.0.1.3 iconos cdn change  1.16

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,23 +1,10 @@
-var usuarioMail;
-auth.onAuthStateChanged(
-  /** Recibe las características del usuario o null si no ha iniciado
-   * sesión. */
-getUserLogin => {
-  if (getUserLogin && getUserLogin.email) {
-    // Usuario aceptado.
-    // @ts-ignore Muestra el email registrado en Google.
-    usuarioMail = getUserLogin.email;
-  } else {
-    // No ha iniciado sesión. Pide datos para iniciar sesión.
-    auth.signInWithRedirect(provider); 
-  }
-},
-// Función que se invoca si hay un error al verificar el usuario. //
-procesaError
-);
 
-var urlNav={estadistics:"",promotions:""}
-  if(usuarioMail=="406474058038.cuponera@gmail.com"){
+auth.onAuthStateChanged(usr=>{infoUser.email=usr.email,infoUser.name=usr.displayName})
+
+var urlNav={promotions: "",estadistics: ""}
+
+function navar(){
+  if(infoUser.email==="406474058038.cuponera@gmail.com"){
     urlNav.estadistics="./estadisticas.html"
     urlNav.promotions="./promociones.html"
   }
@@ -25,15 +12,17 @@ var urlNav={estadistics:"",promotions:""}
     urlNav.estadistics="./estadisticasUsuario.html"
     urlNav.promotions="./promocionesUsuario.html"
   }
+  return urlNav
+}
 
 document.getElementById("navar").innerHTML+=`
 <div class="container-fluid">
       <a class="navbar-brand" href="./index.html">
         <img src="http://1000marcas.net/wp-content/uploads/2020/02/Walmart-logo.png" alt="imagen-walmart" width="90" height="40">
       </a>
-      <a class="navbar-brand text-light" href="${urlNav.promotions}">Promociones</a>
-      <a class="navbar-brand text-light" href="./estadisticas.html">Estadisticas</a>
-      <button class="navbar-brand btn btn btn-link text-light" type="button" onclick="terminaSesión()">Terminar Sesión</button>
+      <a class="navbar-brand" href="${urlNav.promotions}">Promociones</a>
+      <a class="navbar-brand" href="${urlNav.estadistics}">Estadisticas</a>
+      <button class="navbar-brand btn btn btn-link" type="button" onclick="terminaSesión()">Terminar Sesión</button>
 </div>
 `;
   
