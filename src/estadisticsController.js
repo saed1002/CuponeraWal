286e21519ce1bf5aebd7c187d30b4bbc6e4e80ae
Saed1002 <eduardoquet@hotmail.com>
2021-06-15 17:39:07 -0500

    issue

--- src/estadisticsController.js
+++ src/estadisticsController.js
@@ -1,16 +1,4 @@
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyA14Vc16_jkjUzCWBpjwTy_t-Hwtz2Wt5k",
-    authDomain: "cuponerawal.firebaseapp.com",
-    projectId: "cuponerawal",
-    storageBucket: "cuponerawal.appspot.com",
-    messagingSenderId: "406474058038",
-    appId: "1:406474058038:web:62a527c117af27f3b52b97",
-    measurementId: "G-WEEL5GKJB2"
-  };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 //conexion a la bd de firebase
 const db =firebase.firestore()
 const promocion = db.collection("Promociones");
