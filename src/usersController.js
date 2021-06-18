a68d4c43bebbdc2e64d8bf24a09a4fc4d90c3b35
Saed1002 <eduardoquet@hotmail.com>
2021-06-17 22:23:30 -0500

    validaciones

--- src/usersController.js
+++ src/usersController.js
@@ -119,7 +119,7 @@ var validacionUsuario = firebase.auth().onAuthStateChanged(userAuth => {
                     try {
                       console.log(e.target.dataset.id)//id promocion
                       //elimina puntos por uso del cupon y los mete en su wallet
-                      promociones.doc(e.target.dataset.id).onSnapshot(snapshot => {
+                      promocion.doc(e.target.dataset.id).onSnapshot(snapshot => {
                         snapshot.forEach(datos => {
                           var busqueda = datos.data()
                           usuario.doc(registros.id).update({
