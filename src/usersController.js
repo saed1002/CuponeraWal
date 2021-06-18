ab325b3d79d76cad92df4ff1583fa2b9a4eb8855
Saed1002 <eduardoquet@hotmail.com>
2021-06-17 23:24:46 -0500

    validaciones

--- src/usersController.js
+++ src/usersController.js
@@ -14,6 +14,7 @@ var user = firebase.auth().onAuthStateChanged(userAuth => {
   usuario.where("mail", "==", userAuth.email).onSnapshot(async snapshot => {
     if (await snapshot.size >= 1) {
       console.log(snapshot.size)
+      document.getElementById("agregarUsuarios").innerHTML += ''
       document.getElementById("agregarUsuarios").innerHTML += `
             <div class="container">
             <div class="row justify-content-md-center">
@@ -22,6 +23,7 @@ var user = firebase.auth().onAuthStateChanged(userAuth => {
             </div>`;
     }
     else {
+      document.getElementById("agregarUsuarios").innerHTML += '';
       document.getElementById("agregarUsuarios").innerHTML += `
         <div class="container">
         <div class="row justify-content-md-center">
@@ -91,6 +93,7 @@ var validacionUsuario = firebase.auth().onAuthStateChanged(userAuth => {
             if (hoy.toISOString().split(".")[0] <= promociones.timeEnd && usr.points >= promociones.points) {
               var refArch = sg.ref(promociones.rute);
               sg.refFromURL(refArch).getDownloadURL().then(function (url) {
+                document.getElementById("promocionesList").innerHTML += '';
                 document.getElementById("promocionesList").innerHTML += `
       <div class="card mb-3 bg-secondary" style="max-width: 540px;">
                 <div class="row g-0">
@@ -146,6 +149,7 @@ var validacionUsuario = firebase.auth().onAuthStateChanged(userAuth => {
           })
         }
         else {
+          document.getElementById("promocionesList").innerHTML +='';
           document.getElementById("promocionesList").innerHTML += `
               <div class="text-white bg-dark" style="max-width: 540px;">
               <p class="text-center display-4">Proximamente se vendran nuevas promos</p>
