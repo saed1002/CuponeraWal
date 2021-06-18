7f0b4f92aa020be92d031079ef3602f797962861
Saed1002 <eduardoquet@hotmail.com>
2021-06-17 20:10:28 -0500

    validaciones

--- src/usersController.js
+++ src/usersController.js
@@ -78,7 +78,7 @@ var user = firebase.auth().onAuthStateChanged(userAuth => {
 var validacionUsuario = firebase.auth().onAuthStateChanged(userAuth => {
   usuario.where("mail", "==", userAuth.email).onSnapshot(snapshot=>{
     snapshot.forEach(registros=>{
-      var usr= doc.data();
+      var usr= registros.data();
       console.log(usr.name)
 promocion.
   onSnapshot(
