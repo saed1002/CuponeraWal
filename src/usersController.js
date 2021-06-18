e8f552be049fb391b5e1e4adcbc5ec1c920e01a9
Saed1002 <eduardoquet@hotmail.com>
2021-06-17 23:57:41 -0500

    validaciones

--- src/usersController.js
+++ src/usersController.js
@@ -93,7 +93,7 @@ var validacionUsuario = firebase.auth().onAuthStateChanged(userAuth => {
             if (hoy.toISOString().split(".")[0] <= promociones.timeEnd && usr.points >= promociones.points) {
               var refArch = sg.ref(promociones.rute);
               sg.refFromURL(refArch).getDownloadURL().then(function (url) {
-                document.getElementById("promocionesList").innerHTML += '';
+                document.getElementById("promocionesList").innerHTML = '';
                 document.getElementById("promocionesList").innerHTML += `
       <div class="card mb-3 bg-secondary" style="max-width: 540px;">
                 <div class="row g-0">
@@ -149,7 +149,7 @@ var validacionUsuario = firebase.auth().onAuthStateChanged(userAuth => {
           })
         }
         else {
-          document.getElementById("promocionesList").innerHTML +='';
+          document.getElementById("promocionesList").innerHTML ='';
           document.getElementById("promocionesList").innerHTML += `
               <div class="text-white bg-dark" style="max-width: 540px;">
               <p class="text-center display-4">Proximamente se vendran nuevas promos</p>
