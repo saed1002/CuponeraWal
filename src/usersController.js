ac950433d1c68c527efde0421302a2ea8c1df2cc
Saed1002 <eduardoquet@hotmail.com>
2021-06-18 23:43:23 -0500

    check

--- src/usersController.js
+++ src/usersController.js
@@ -134,9 +134,12 @@ var validacionUsuario = firebase.auth().onAuthStateChanged(userAuth => {
                       const infoCupones= await getCupones(registros.id)
                       console.log(infoCupones.data)
                       const cuponesAlmacenados=infoCupones.data()
-                      console.log(cuponesAlmacenados.coupons);
+                      console.log("historico de cupones "+cuponesAlmacenados.coupons);
+                      console.log("cupon seleccionado: "+e.target.dataset.id);
                       colecccionCupones[colecccionCupones]=cuponesAlmacenados.coupons
+                      console.log("primer guardado de cupones del usuario "+colecccionCupones);
                       colecccionCupones[colecccionCupones]=e.target.dataset.id
+                      console.log("segundo guardado de cupones de la promo "+colecccionCupones);
                             console.log(colecccionCupones);
                                 usuario.doc(registros.id).update({
                                   coupons: colecccionCupones,
